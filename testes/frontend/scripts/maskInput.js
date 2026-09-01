c385f1e776992ab6f85af1bd1e00e61ba0ae8442
Camilly Pessotti <camillypessotti05@gmail.com>
2026-09-01 19:33:07 -0300

    fix: update input types for CPF, phone number, and CEP fields; add address autofill functionality

--- testes/frontend/scripts/maskInput.js
+++ testes/frontend/scripts/maskInput.js
@@ -3,23 +3,17 @@ const phoneNumberInput = document.getElementById("phoneNumberInput");
 const cepInput = document.getElementById("cepInput");
 
 function inputMask(input, pattern) {
-    console.log("input:\n" + input);
-    console.log("pattern:\n" + pattern);
     input.addEventListener('input', () => {
         let value = input.value.replace(/\D/g, '');
-        console.log("value:\n" + value);
         let newValue = 0;
         let result = '';
 
         for (let i = 0; i < pattern.length && newValue < value.length; i++) {
             if (pattern[i] === '9') {
                 result += value[newValue];
-                console.log("result:\n" + result);
                 newValue++;
-                console.log("newValue:\n" + newValue);
             } else {
                 result += pattern[i];
-                console.log("result:\n" + result);
             };
         };
 
@@ -30,3 +24,40 @@ function inputMask(input, pattern) {
 inputMask(cpfInput, '999.999.999-99');
 inputMask(phoneNumberInput, '(99) 99999-9999');
 inputMask(cepInput, '99999-999');
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+const countryInput = document.getElementById("countryInput");
+const stateInput = document.getElementById("stateInput");
+const cityInput = document.getElementById("cityInput");
+const neighborhoodInput = document.getElementById("neighborhoodInput");
+const streetInput = document.getElementById("streetInput");
+
+cepInput.addEventListener('blur', async () => {
+    const cep = cepInput.value.replace(/\D/g, '');
+
+    if (cep.length !== 8) {
+        return;
+    }
+
+    try {
+        // https://viacep.com.br/
+        const response = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+        const data = await response.json();
+
+        if (data.erro) {
+            alert('404\nCEP not found');
+            return;
+        }    
+
+        countryInput.value = "Brasil"
+        stateInput.value = data.uf;
+        cityInput.value = data.localidade;
+        neighborhoodInput.value = data.bairro;
+        streetInput.value = data.logradouro;
+
+    } catch (error) {
+        console.error('Error trying to get CEP', error);
+        alert('500\nServidor Error');
+    }
+});
