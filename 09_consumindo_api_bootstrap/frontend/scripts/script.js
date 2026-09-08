e23772487e0d55a32cfd14b2a6e9316a7b867ee0
pessotticamilly <camillypessotti05@gmail.com>
2026-09-08 20:15:33 -0300

    feat: arrumando o scritp

--- 09_consumindo_api_bootstrap/frontend/scripts/script.js
+++ 09_consumindo_api_bootstrap/frontend/scripts/script.js
@@ -5,13 +5,18 @@ const bairro = document.getElementById("bairro");
 const rua = document.getElementById("rua");
 
 cep.addEventListener("blur", async () => {
-    // const response = await fetch(url)
-    // viacep.com.br/ws/01001000/json/
-    const response = await fetch(`https://viacep.com.br/ws/${cep.value}/json/`);
-    const data = await response.json();
+    try {
+        // const response = await fetch(url)
+        // viacep.com.br/ws/01001000/json/
+        const response = await fetch(`https://viacep.com.br/ws/${cep.value}/json/`);
+        const data = await response.json();
 
-    estado.value = data.estado;
-    cidade.value = data.localidade;
-    bairro.value = data.bairro;
-    rua.value = data.logradouro;
+        estado.value = data.estado;
+        cidade.value = data.localidade;
+        bairro.value = data.bairro;
+        rua.value = data.logradouro
+    } catch (error) {
+        console.log("Erro tentando buscar CEP:\n" + error);
+        alert("500\nServidor Error")
+    }
 });
