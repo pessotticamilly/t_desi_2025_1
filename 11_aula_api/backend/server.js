1e35e1c3c2726e047d08aaf8352b1880f61295c3
pessotticamilly <camillypessotti05@gmail.com>
2026-09-23 18:19:27 -0300

    refactor: changing listen message

--- 11_aula_api/backend/server.js
+++ 11_aula_api/backend/server.js
@@ -10,5 +10,5 @@ app.use(produtosRoute);
 app.use(clientesRoute);
 
 app.listen(PORT, () => {
-    console.log(`Servidor rodando em http://localhost:${PORT}`)
+    console.log(`Servidor rodando na porta: ${PORT}\nhttp://localhost:${PORT}`)
 });
